251b26a61a64d3a25db3fcc36f12589d6fcb883a
Hosain Ibna Bashar <hosain.cse2@gmail.com>
2026-03-05 22:43:29 +0100

    redesign the header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,12 +1,34 @@
-import { Link } from "react-router-dom";
-import "./Header.module.css";
+import { Link, useLocation } from "react-router-dom";
+import styles from "./Header.module.css";
+import logo from "@/assets/logo.png";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
-    <header>
-      <nav>
-        <Link to="/map">Map</Link>
-        <Link to="/dashboard">Dashboard</Link>
+    <header className={styles.header}>
+      <Link to="/" className={styles.logo}>
+        <img src={logo} alt="Logo" className={styles.logoImage} />
+      </Link>
+      <nav className={styles.nav}>
+        <Link
+          to="/map"
+          className={
+            location.pathname === "/" || location.pathname === "/map"
+              ? styles.active
+              : undefined
+          }
+        >
+          Map
+        </Link>
+        <Link
+          to="/dashboard"
+          className={
+            location.pathname === "/dashboard" ? styles.active : undefined
+          }
+        >
+          Dashboard
+        </Link>
       </nav>
     </header>
   );
